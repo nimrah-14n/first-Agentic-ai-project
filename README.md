d124fec8a475eca1dae1409d7afb0879f2b654aa
Nimrah Hussain <160315958+nimrah-14n@users.noreply.github.com>
2025-06-01 15:22:07 +0500

    Add files via upload

--- README.md
+++
@@ -1,1 +0,0 @@
-# first-Agentic-ai-project
---
+++ main.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import os
+from agents import Agent, Runner,AsyncOpenAI,OpenAIChatCompletionsModel, RunConfig
+
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+agent = Agent(
+    name = "Translator",
+    instructions = "you are a translator agent that tranlsates text from english to urdu."
+)
+
+response = Runner.run_sync(
+    agent,
+    input ="Hello, how are you?",
+    run_config = config
+)
+
+print(response)
